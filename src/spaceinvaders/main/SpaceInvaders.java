e3e090bd7e632ea1d3b50323d7152bfec689a870
Thillo <tgilles2002@gmail.com>
2024-05-27 17:55:17 +0200

    main package kommentiert

--- src/spaceinvaders/main/SpaceInvaders.java
+++ src/spaceinvaders/main/SpaceInvaders.java
@@ -2,55 +2,61 @@ package spaceinvaders.main;
 
 import spaceinvaders.gui.GameFrame;
 
-
 public final class SpaceInvaders {
 
-    // Sagt an ob das Spiel läuft
+    // Flag, das angibt, ob das Spiel läuft
     public boolean running = false;
-    // Instanz des Spiels
+    // Statische Instanz des Spiels
     public static SpaceInvaders instance;
+    // Das Hauptfenster des Spiels
     public GameFrame frame;
 
+    // Konstruktor des Spiels
     public SpaceInvaders() {
         // Setzen der Instanz
         instance = this;
 
-         //aufrufen der Methoden
+        // Aufrufen der Methoden zur Initialisierung, Hauptschleife und Stoppen des Spiels
         initGame();
         gameLoop();
         stopGame();
-
     }
 
+    // Initialisiert das Spiel
     private void initGame() {
+        // Erstellen eines neuen Spielrahmens mit einer bestimmten Größe
         frame = new GameFrame(800, 550);
-        // Hier kommt die Initialisierung hin
+        // Setzen des Running-Flags auf true
         running = true;
     }
 
+    // Die Hauptspielschleife
     private void gameLoop() {
         while (running) {
-            // Hier kommt die Spiellogik hin
+            // Aktualisieren des Spielframes
             frame.update();
+            // Neuzeichnen des Spielframes
             frame.repaint();
+            // Ein kurzes Warten, um die CPU nicht zu stark zu belasten
             try {
                 Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //Testausgabe
-            //System.out.print("Game Loop");
+            // Testausgabe (kann entfernt werden)
+            // System.out.print("Game Loop");
         }
     }
 
+    // Stoppt das Spiel
     private void stopGame() {
+        // Beendet das Programm
         System.exit(0);
     }
 
+    // Der Einstiegspunkt des Programms
     public static void main(String[] args) {
-
+        // Starten einer neuen Instanz des Spiels
         new SpaceInvaders();
-
     }
-
 }
